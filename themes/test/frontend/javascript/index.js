b36d29c3bbf648eedbb1efa24d9742262d174c99
vovawolf <14vova10@gmail.com>
2017-03-05 19:16:46 +0200

    test

--- themes/test/frontend/javascript/index.js
+++ themes/test/frontend/javascript/index.js
@@ -3,5 +3,57 @@
  */
 
 (() => {
-    console.log('init');
+	const $window = $(window);
+	const $body = $('body');
+
+	class App {
+		initSliders = () => {
+			const $slickSlider = $('#responsive');
+			const $item = $slickSlider.find('.item');
+
+			$slickSlider.slick({
+				dots: true,
+				infinite: false,
+				slide: '.item',
+				speed: 300,
+				slidesToShow: 4,
+				slidesToScroll: 4,
+			});
+
+			$item.on('click', function () {
+				console.log($(this));
+			});
+		};
+
+		buildMainVav = () => {
+			console.log('nav');
+		};
+
+		init = () => {
+			this.initSliders();
+			this.buildMainVav();
+		};
+
+		handleScroll = () => {
+			console.log('scroll');
+		};
+
+		handleResize = () => {
+			console.log('resize');
+		};
+
+		destroy = () => {
+			console.log('destriyed my app');
+		};
+	}
+
+	$(() => {
+		const MyApp = new App();
+
+		MyApp.init();
+
+		$window
+			.on('scroll', MyApp.handleScroll)
+			.on('resize', MyApp.handleResize)
+	});
 })();
