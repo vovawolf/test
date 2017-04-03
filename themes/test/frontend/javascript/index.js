5177773fe7d02d18975dfb70a9b245a89720247f
vovawolf <14vova10@gmail.com>
2017-04-03 19:27:40 +0300

    first commit

--- themes/test/frontend/javascript/index.js
+++ themes/test/frontend/javascript/index.js
@@ -8,16 +8,19 @@
 
 	class App {
 		initSliders = () => {
-			const $slickSlider = $('#responsive');
+			const $slickSlider = $('.sl');
 			const $item = $slickSlider.find('.item');
 
 			$slickSlider.slick({
+				autoplay: true,
+				autoplaySpeed: 5000,
 				dots: true,
-				infinite: false,
-				slide: '.item',
-				speed: 300,
-				slidesToShow: 4,
-				slidesToScroll: 4,
+				arrows: false,
+				fade: true,
+				pauseOnDotsHover: true,
+				pauseOnFocus: false,
+				pauseOnHover: false,
+				speed: 1000,
 			});
 
 			$item.on('click', function () {
@@ -47,6 +50,7 @@
 		};
 	}
 
+
 	$(() => {
 		const MyApp = new App();
 
@@ -57,3 +61,94 @@
 			.on('resize', MyApp.handleResize)
 	});
 })();
+
+
+(() => {
+	const $window = $(window);
+	const $body = $('body');
+
+	class App {
+		initSliders = () => {
+			const $slickSlider = $('.sl1');
+			const $item = $slickSlider.find('.item1');
+
+			$slickSlider.slick({
+				autoplay: true,
+				autoplaySpeed: 8000,
+				dots: true,
+				arrows: false,
+				pauseOnDotsHover: true,
+				pauseOnFocus: false,
+				pauseOnHover: false,
+				speed: 1000,
+				slidesToShow: 2,
+				infinite: true,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 1,
+						}
+					},
+				]
+			});
+
+			$item.on('click', function () {
+				console.log($(this));
+			});
+		};
+
+		buildMainVav = () => {
+			console.log('nav');
+		};
+
+		init = () => {
+			this.initSliders();
+			this.buildMainVav();
+		};
+
+		handleScroll = () => {
+			console.log('scroll');
+		};
+
+		handleResize = () => {
+			console.log('resize');
+		};
+
+		destroy = () => {
+			console.log('destriyed my app');
+		};
+	}
+
+
+	$(() => {
+		const MyApp = new App();
+
+		MyApp.init();
+
+		$window
+			.on('scroll', MyApp.handleScroll)
+			.on('resize', MyApp.handleResize)
+	});
+})();
+
+$(document).ready(function() {
+
+	$('.menu__list>.menu__drop').hide();
+
+
+	$('.menu__list>a').click(function() {
+
+		var findArticle = $(this).next();
+		var findWrapper = $(this).closest('.menu__list');
+
+		if (findArticle.is(':visible')) {
+			findArticle.slideUp('300');
+		}
+		else {
+			findWrapper.find('>article').slideUp('fast');
+			findArticle.slideDown('fast');
+		}
+	});
+
+});
